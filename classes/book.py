2e9a4047b609888236eb6e6016de3667af21be26
Idan <idansano10@gmail.com>
2025-11-10 17:31:59 +0200

    I changed book class in main branch in mestake

--- classes/book.py
+++ classes/book.py
@@ -1,6 +1,12 @@
 class Book:
-    def __init__(self,title:str,author:str,ISBN:int,is_available:bool):
-        self.title=title
-        self.author=author
-        self.ISBN=ISBN
-        self.is_avilable=is_available
+    def __init__(self , title : str, author : str, isbn : str):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.isavailable = True
+
+    def __str__(self):
+        return f"This book called {self.title} ,its author is {self.author}, its isbn is {self.isbn} and is available = {self.isavailable}"
+
+    def is_available(self):
+        return self.isavailable
