9c43d9141581c011b2cae03ae256c135303e5409
mendy hershkop <hers.mm@gmail.com>
2025-11-11 09:52:09 +0200

    create Json load and write

--- classes/book.py
+++ classes/book.py
@@ -1,6 +1,11 @@
+import json
 class Book:
-    def __init__(self,title:str,author:str,ISBN:int,is_available:bool):
+    def __init__(self,title:str,author:str,ISBN:int):
         self.title=title
         self.author=author
         self.ISBN=ISBN
-        self.is_avilable=is_available
+        self.is_avilable=True
+    def book_save(self,json_file):
+        with open(json_file, "w") as file:
+            json_book=json.dump(({"title":self.title,"author":self.author,"ISBN":self.ISBN}), file)
+            file.write(json_book)
--- json_files/json_creater.py
+++ json_files/json_creater.py
@@ -1,9 +1,6 @@
 import json
-
-class JasonSapared:
-    def book_created(self,json_file):
-        with open(json_file,"r") as file:
-            for i in file:
-                new_book=Book(i['title'],i['auther'],i['ISBN'],True)
-book=JasonSapared
-book.book_created("json_file.json")
+class JsonCreater:
+    @staticmethod
+    def create(json_file,data):
+        with open(json_file,"a") as file:
+            file.append(json.dumps(data))
